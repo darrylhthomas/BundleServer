1b37ff0e90fce31cca08f815bbc55c2604991053
Darryl H. Thomas <darrylhthomas@me.com>
2013-03-09 18:36:35 -0800

    Initial Commit

---
+++ Bundle Server/BDLDocument.h
@@ -0,0 +1,13 @@
+//
+//  BDLDocument.h
+//  Bundle Server
+//
+//  Created by Darryl H. Thomas on 3/9/13.
+//  Copyright (c) 2013 Darryl H. Thomas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BDLDocument : NSDocument
+
+@end
---
+++ Bundle Server/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Bundle Server
+//
+//  Created by Darryl H. Thomas on 3/9/13.
+//  Copyright (c) 2013 Darryl H. Thomas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
